c9444fb1ffd1cdf2ae1c42e4c538500a1c7e4a5d
gagan gupta <gagan202020@gmail.com>
2023-11-20 10:36:18 +0530

    classes

--- src/main/java/org/example/customer/CustomerController.java
+++ src/main/java/org/example/customer/CustomerController.java
@@ -1,12 +1,12 @@
-package org.example.vendor;
+package org.example.customer;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Vendor {
+public class CustomerController {
 
-    @RequestMapping("/")
+    @RequestMapping("/customers")
     String hello() {
         return "Hello World, Spring Boot!";
     }
---
+++ src/main/java/org/example/vendor/VendorController.java
@@ -0,0 +1,14 @@
+package org.example.vendor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VendorController {
+
+    @RequestMapping("/vendors")
+    String hello() {
+        return "Hello World, Spring Boot!";
+    }
+
+}
